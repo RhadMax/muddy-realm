70420b315aaf850f491a44f2aad4ecc0d914a50a
Max Patten <amanthus2535@gmail.com>
2019-06-25 22:59:32 -0700

    started setting up inquirer blocks but stuck on confirmation concept with readback data, maybe promises

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var items = require("./items.js");
 var characters = require("./characters.js");
+var mobs = require("./mobs.js");
+var inquirer = require("inquirer");
 
 // console.log(items);
 // console.log(characters);
@@ -7,9 +9,74 @@ var characters = require("./characters.js");
 
 const findIndex = (name, array) => {
     for (let i = 0; i < array.length; i++) {
-        if (array[i].name.toLowerCase() === name){ return array[i]}
+        if (array[i].name.toLowerCase() === name) {
+            return array[i]
+        }
     }
 }
-let max = findIndex("max", characters)
-console.log(max)
-console.log(max.speed())
+const mainMenu = function () {
+    inquirer.prompt([
+        {
+            type: "list",
+            choices: ["New Game", "Load Character", "Exit Game"],
+            message: "Welcome to the Muddy Realms! Which road will you choose?",
+            name: "menu"
+        }
+    ]).then((res) => {
+        let choice = res.menu;
+        switch (choice) {
+            case "New Game":
+                //all broken in here... promise maybe??
+                var confirmNewGame = new Promise(confirm(res).then(function(){
+                    console.log("debug")
+                    switch (confirmNewGame) {
+                        case "Yes":
+                            intro();
+                        case "No":
+                            console.log("Very well, returning to main menu.");
+                            mainMenu();
+
+                    };
+                });
+            case "Load Character":
+                ;
+            case "Exit Game":
+                ;
+        }
+    });
+};
+const confirm = function (res) {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Looks like you selected: \n" + JSON.stringify(res, null, 2) + "\nDoes this look correct?",
+            choices: ["Yes", "No"],
+            name: "selection"
+        }
+    ]).then((answer) => {
+        let confirmation = answer.selection;
+        switch (confirmation) {
+            case "Yes":
+                confirmNewGame = "Yes";
+            case "No":
+                confirmNewGame = "No";
+        }
+        return confirmNewGame;
+    });
+    
+};
+const combat = function () {
+    inquirer.prompt([
+        {
+
+        }
+    ]).then((res) => {
+
+    });
+};
+
+mainMenu();
+
+// let max = findIndex("max", characters)
+// console.log(max)
+// console.log(max.speed())
--- mobs.js
+++ mobs.js
@@ -1,1 +1,10 @@
 const construct = require("./constructors.js");
+
+var mobs = [];
+
+let fox = new construct.Character("fox", "Slinking red fox", "beast", 4, 5, 2, "mangy", "red", "yellow", "slim", 2, 6, 6, 8, 5, 3, 3, 4, 10, 20, 5, 50)
+
+
+// console.log(fox.speed())
+mobs.push(fox);
+module.exports = mobs;
